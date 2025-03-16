508e3fb4e91c27ff9be95b41fdc15188c4886013
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2025-03-17 00:01:15 +1000

    initial project code

---
+++ unifiedcatalogpy/__init__.py
@@ -0,0 +1,3 @@
+from .utils import format_api_endpoint
+
+__version__ = "0.0.1"
---
+++ unifiedcatalogpy/utils.py
@@ -0,0 +1,13 @@
+from typing import Literal
+
+
+def format_base_url(account_id: str) -> str:
+    """
+    Format the base URL for the Unified Catalog API.
+
+    :param account_id: The guid of the Microsoft Purview account.
+    :return: The formatted base URL.
+    """
+    return (
+        f"https://{account_id}-api.purview-service.microsoft.com/datagovernance/catalog"
+    )
